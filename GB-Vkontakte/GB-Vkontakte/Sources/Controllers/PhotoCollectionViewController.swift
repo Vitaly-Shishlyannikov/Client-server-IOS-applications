fc6971ef27e1bc24d4e7a340692cefa5501e8b00
Vit_sh <vithits@yandex.ru>
2019-06-27 17:23:40 +0300

    Добавлен класс Session

---
+++ GB-Vkontakte/GB-Vkontakte/Sources/Controllers/PhotoCollectionViewController.swift
@@ -0,0 +1,97 @@
+//
+//  PhotoCollectionViewController.swift
+//  GB-Vkontakte
+//
+//  Created by Vitaly_Shishlyannikov on 27.05.2019.
+//  Copyright © 2019 Vit. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+    var friendNameForTitle: String = ""
+    
+    var photos: [PhotoModel] = []
+    
+    func getPhotos () {
+        photos = PhotosServerEmulator.getPhotos() ?? []
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getPhotos()
+        
+        title = friendNameForTitle
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return photos.count
+
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else {return UICollectionViewCell()}
+        
+        let photoPath = photos[indexPath.row].photoPath
+        cell.photoImageView.image = UIImage(named: photoPath)
+
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "fullScreenSegue",
+            let fullScreenPhoto = segue.destination as? FullScreenPhotoController,
+            let indexPaths = collectionView.indexPathsForSelectedItems,
+            let indexPath = indexPaths.first {
+                fullScreenPhoto.indexPathSelected = indexPath
+            }
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ GB-Vkontakte/GB-Vkontakte/Sources/Singleton/SessionSingleton.swift
@@ -0,0 +1,20 @@
+//
+//  SessionSingleton.swift
+//  GB-Vkontakte
+//
+//  Created by Vitaly_Shishlyannikov on 27.06.2019.
+//  Copyright © 2019 Vit. All rights reserved.
+//
+
+import UIKit
+
+class Session {
+    
+    static let instance = Session()
+    
+    private init(){}
+    
+    var token: String = ""
+    var userId: Int = 0
+    
+}
